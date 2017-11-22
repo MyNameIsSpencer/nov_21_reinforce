f4c6a901e9ad4fb20e8b71ebd23213de4bb66b91
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-21 21:01:21 -0500

    done reinforcement exercise

---
+++ nov_21_reinforce.rb
@@ -0,0 +1,33 @@
+def select_cards(possible_cards, hand)
+
+  possible_cards.each do |current_card|
+    print "Do you want to pick up #{current_card}?"
+    answer = gets.chomp
+    if answer.downcase == 'y'
+      hand << current_card
+    end
+  end
+  return hand
+
+
+end
+
+available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of spades', 'queen of hearts']
+hander = []
+new_hand = select_cards(available_cards, hander)
+
+display_hand = new_hand.join("\n")
+
+puts "Your new hand is: \n#{display_hand}"
+
+# Its output should look something like this:
+#
+# Do you want to pick up queen of spades?y
+# Do you want to pick up 2 of clubs?n
+# Do you want to pick up 3 of diamonds?n
+# Do you want to pick up jack of spades?y
+# Do you want to pick up queen of hearts?y
+# Your new hand is:
+# queen of spades
+# jack of spades
+# queen of hearts
